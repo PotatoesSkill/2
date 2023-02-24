26b9e8e4012051f059d334284195352e74953a6d
PotatoesSkill <75749677+PotatoesSkill@users.noreply.github.com>
2023-02-23 23:07:01 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-fetch("https://potatoesskill.com/api/ryuukapatreons").then(res => {
-                return res.text();
-            }).then(data => {
-                document = data
-                console.log(data)
-})
